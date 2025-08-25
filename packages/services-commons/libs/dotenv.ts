da6d292bf03c521a59b012794fb58e95c8109b13
Raghvendra Misra <itsraghav12@gmail.com>
2025-08-25 23:08:00 +0530

    Refactor: Update .gitignore to simplify environment file exclusions and fix dotenv path reference

--- packages/services-commons/libs/dotenv.ts
+++ packages/services-commons/libs/dotenv.ts
@@ -3,6 +3,6 @@ import {resolve} from "path";
 import { cwd } from "process";
 
 export function loadEnv(){
-    config({path: resolve(__dirname, "../../../root.env")})
+    config({path: resolve(__dirname, "../../../.env.root")})
     config({path:resolve(cwd(), ".env")})
 }
