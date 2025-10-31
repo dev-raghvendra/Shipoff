7d7fc25e48fd547faa87a26c9a06237094885c00
Raghvendra Misra <itsraghav12@gmail.com>
2025-11-01 03:12:30 +0530

    refactor: remove unused getK8Deployment method and update service images to version 2.0.0 across deployments

--- apps/services/gateway/src/services/orchestrator.service.ts
+++ apps/services/gateway/src/services/orchestrator.service.ts
@@ -1,21 +1,11 @@
 import { GetOrchestratorClient } from "@shipoff/grpc-clients";
-import { IGetCloneURIRequest, IStartK8DeploymentRequest, OrchestratorServiceClient } from "@shipoff/proto";
+import { IGetCloneURIRequest, OrchestratorServiceClient } from "@shipoff/proto";
 import { promisifyGrpcCall } from "@shipoff/services-commons";
 
 export class OrchestratorService {
   private _orchestratorService : OrchestratorServiceClient
   constructor() {
     this._orchestratorService = GetOrchestratorClient();
-  }
-
-  async getK8Deployment(body:any){
-    try {
-        const req = IStartK8DeploymentRequest.fromObject(body)
-        const response = await promisifyGrpcCall(this._orchestratorService.IStartK8Deployment.bind(this._orchestratorService), req);
-        return response;
-      } catch (e:any) {
-        return e;
-    }
   }
 
   async getCloneURI(body:any){
