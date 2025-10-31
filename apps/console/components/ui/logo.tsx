cb366ac662943f41db2bb8326b0e86cab2e00528
Raghvendra Misra <itsraghav12@gmail.com>
2025-11-01 03:50:41 +0530

    logo fixed

--- apps/console/components/ui/logo.tsx
+++ apps/console/components/ui/logo.tsx
@@ -1,8 +1,20 @@
+"use client";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export function Logo({ className="h-10 w-auto" }: { className?: string }) {
     const {resolvedTheme} = useTheme();
-    const src = resolvedTheme === "dark" ? "/meta/logo-dark.svg" : "/meta/logo-light.svg";
+    const [src, setSrc] = useState("/meta/logo-light.svg");
 
-    return <img className={className} src={src} alt="Logo" />;
+    useEffect(() => {
+      const newSrc = resolvedTheme === "dark" ? "/meta/logo-dark.svg" : "/meta/logo-light.svg";
+      setSrc(newSrc);
+    }, [resolvedTheme]);
+    return (
+      <img
+        src={src}
+        alt="Logo"
+        className={className}
+      />
+    );
 }
--- apps/landing/components/ui/logo.tsx
+++ apps/landing/components/ui/logo.tsx
@@ -1,10 +1,15 @@
-"use client"
+"use client";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export function Logo({ className="h-10 w-auto" }: { className?: string }) {
     const {resolvedTheme} = useTheme();
-    const src = resolvedTheme === "dark" ? "/meta/logo-dark.svg" : "/meta/logo-light.svg";
+    const [src, setSrc] = useState("/meta/logo-light.svg");
 
+    useEffect(() => {
+      const newSrc = resolvedTheme === "dark" ? "/meta/logo-dark.svg" : "/meta/logo-light.svg";
+      setSrc(newSrc);
+    }, [resolvedTheme]);
     return (
       <img
         src={src}
